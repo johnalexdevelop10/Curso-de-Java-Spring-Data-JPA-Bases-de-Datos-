85782ed64c19d5d40a3b0ad37e64c76e0e46c0ef
Johndev <johnzeta10@gmail.com>
2023-10-06 20:00:41 -0500

    llegando a 10

--- src/main/java/com/platzi/pizza/service/PizzaService.java
+++ src/main/java/com/platzi/pizza/service/PizzaService.java
@@ -6,6 +6,7 @@ import com.platzi.pizza.persitence.repository.PizzaRepository;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,4 +25,14 @@ public class PizzaService {
     public PizzaEntity get(int idPizza){
         return this.pizzaRepository.findById(idPizza).orElse(null);
     }
+
+    public PizzaEntity save(PizzaEntity pizza){
+        return this.pizzaRepository.save(pizza);
+    }
+
+    public boolean exists(int idPizza){
+        return this.pizzaRepository.existsById(idPizza);
+    }
+
+
 }
